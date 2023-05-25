1d9004b3deb23e0d486ea8711f1da2ff199bc830
Njaaga-Gakure <50949648+Njaaga-Gakure@users.noreply.github.com>
2023-05-25 17:36:10 +0300

    add container for categories

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,10 +1,13 @@
 const Categories = ({categories, filterCategories}) => {
     return (
-        <div className="categories">
-            {categories.map((category, index) => {
-                return <button onClick={() => filterCategories(category)} key={index} className="category-btn">{category}</button>
-            })}
-        </div>
+        <div className="categories-container">
+            <p className="categories-text">what will you be having?</p>
+            <div className="categories">
+                {categories.map((category, index) => {
+                    return <button onClick={() => filterCategories(category)} key={index} className="category-btn">{category}</button>
+                })}
+            </div>
+        </div>       
     )
 }
 
